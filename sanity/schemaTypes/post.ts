e8ec7afc39dfef170cf817f9bcea4f569d7b75b3
Suzie Kim <suuuzie18@gmail.com>
2024-08-06 11:24:38 +1000

    chore: remove unused post

--- sanity/schemaTypes/post.ts
+++ sanity/schemaTypes/post.ts
@@ -29,7 +29,7 @@ export const post = {
       name: "excerpt",
       title: "Excerpt",
       type: "text",
-      validation: (Rule: Rule) => Rule.max(200).error("Max 200 characters"),
+      validation: (Rule: Rule) => Rule.max(300).error("Max 300 characters"),
     },
     {
       name: "body",
