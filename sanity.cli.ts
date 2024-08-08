902d428a0153202eea87c8182fe761c2e66af546
Suzie Kim <suuuzie18@gmail.com>
2024-08-08 18:03:01 +1000

    Fix Sanity error

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,8 @@
 import { defineCliConfig } from "sanity/cli";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.SANITY_STUDIO_DATASET;
+const dataset =
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET ||
+  process.env.SANITY_STUDIO_DATASET;
 
 export default defineCliConfig({ api: { projectId, dataset } });
