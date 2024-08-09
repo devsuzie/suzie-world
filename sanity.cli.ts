a777239f0fa2973d9008bca53f0695b3e696cc4e
Suzie Kim <suuuzie18@gmail.com>
2024-08-09 15:47:37 +1000

    Update enviornment variables

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,9 +4,7 @@
  **/
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset =
-  process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET ||
-  process.env.SANITY_STUDIO_DATASET;
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET || "production";
 
 export default defineCliConfig({ api: { projectId, dataset } });
--- sanity/env.ts
+++ sanity/env.ts
@@ -2,13 +2,13 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-07-10";
 
 export const dataset = assertValue(
-  "production",
+  process.env.SANITY_STUDIO_DATASET || "production",
   "Missing environment variable: SANITY_STUDIO_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+  process.env.SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: SANITY_STUDIO_PROJECT_ID"
 );
 
 export const useCdn = false;
